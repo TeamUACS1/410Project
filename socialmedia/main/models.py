3a478229df6798525190036b1f66dda2dc131993
Pranjali Pokharel <pranjali@ualberta.ca>
2015-03-05 10:49:08 -0700

    Added some more models

--- socialmedia/main/models.py
+++ socialmedia/main/models.py
@@ -8,6 +8,14 @@ class Posts(models.Model):
 	extra = models.CharField(max_length=32)
 
 	def __unicode__(self):
-		return self.title
+		return self.author
 
-	
+class Users(models.Model):
+	username = models.CharField(max_length=256, null=Flase)
+	#image = models.ImageField(upload_to = 'images/profile_Img/')
+
+class Friends(models.Model):
+	username1=models.CharField(max_length=256, null=False)
+	#username1=models.ForeignKey(Users, null=False)
+	username2=models.CharField(max_length=256, null=False)
+	#username2=models.ForeignKey(Users, null=False)
