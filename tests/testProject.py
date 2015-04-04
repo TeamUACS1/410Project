11160c11506f39812836f8eba9e7984fa1e89ba9
Curth <curnow@ualberta.ca>
2015-04-04 16:18:58 -0600

    added documentation to tests

--- tests/testProject.py
+++ tests/testProject.py
@@ -4,44 +4,50 @@ from main.models import Authors,Posts,Friends
 from main.views import getGithubActivity
 
 class ProjectTestCase(TestCase):
+    """
+    This file tests socialmedia's models and views, these are what
+    makes the bulk of our project
+    """
     def setUp(self):
         self.client = Client()
-	#self.response = self.client.post('/socialmedia/login', {'admin': 'admin', 'admin':'admin'})
-	self.username1 = 'samohT'	
+	self.username1 = 'samohT'
+	self.guid1 = "id123"	
 	self.github1 = 'curopium'
-	self.user1 = Authors(displayname = self.username1)
+	self.user1 = Authors(displayname = self.username1, guid = self.guid1)
 	self.postText1 = "The quick brown fox jumps over the lazy dog"
 	self.post1 = Posts(author = self.username1, content = self.postText1)
 	self.username2 = 'ekiM'
-	self.user2 = Authors(displayname = self.username2)
-	#self.friends1 = Friends(username1 = self.username1, username2 = self.username2)
+	self.guid2 = "otherid124"
+	self.user2 = Authors(displayname = self.username2, guid= self.guid2)
+	self.friends1 = Friends(authorguid1 = self.guid1,authorguid2 = self.guid2)
 
     def test_the_tests(self):
+        """simply a test to make sure the tests are functional"""
         self.assertEqual(True,True)
 
     def test_user_creation(self):
+        """Creates a user and checks to see if the name matches"""
 	self.assertEqual(self.user1.displayname,self.username1) 
-	
     def test_post_creation(self):
+        """Creates a Post and checks to see if the content matches"""
         self.assertTrue((self.post1.author == self.username1) and (self.post1.content == self.postText1))
 	
-    #def test_friend_creation(self):
-	#self.assertTrue((self.friends1.username1 == self.username1) and (self.friends1.username2 == self.username2))   
+    def test_friend_creation(self):
+        """Checks to see if the friend class has matching guids"""
+	self.assertTrue((self.friends1.authorguid1 == self.guid1) and (self.friends1.authorguid2 == self.guid2))   
 
     #expected to not return anything because github name not set
     def test_github_githubUsernam_check(self):
+        """This test to make sure that a none-existant github username dosent return anything"""
 	self.user1.github = ""
         self.assertEqual(getGithubActivity(self.user1), '')
 
-    #will not work if you cant connect to github
     def test_github_activity_get(self):
+        """
+	Test will fail if you cant connect to github.
+	This test checks one of the team-members github account
+        and returns any GitHub activity from that user. 
+        """
 	self.user1.github = self.github1
         self.assertTrue(getGithubActivity(self.user1))
 
-    """
-    def test_login(self):
-        newresponse = self.client.post('/socialmedia/login', {'username': 'admin', 'password':'admin'}) 
-	self.assertEquals(newresponse.status_code, 200)
-	print("=-=-=-=-=-=-=-=-=")
-	print(newresponse)
-    """
--- tests/testResponse.py
+++ tests/testResponse.py
@@ -6,16 +6,20 @@ import unittest
 BASEURL = "http://127.0.0.1:8000"
 
 class TestYourWebserver(unittest.TestCase):
+    """
+    Tests some the responses from the server with urllib2
+    """
     def setUp(self,baseurl=BASEURL):
-        """do nothing"""
         self.baseurl = baseurl
 
     def test_get_indexhtml(self):
+        """Tests to see if we can get 200 OK from the mainpage"""
         url = self.baseurl + "/main"
         req = urllib2.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
 
     def test_get_404(self):
+        """This tests for a 404 error for a page that dosent exist"""
         url = self.baseurl + "/do-not-implement-this-page-it-is-not-found"
         try:
             req = urllib2.urlopen(url, None, 3)
@@ -25,18 +29,6 @@ class TestYourWebserver(unittest.TestCase):
         else:
             self.assertTrue( False, "Another Error was thrown!")
         
-    """
-    commented out because causes server errors
-    def test_get_login_error(self):
-        url = self.baseurl + "/socialmedia/loggedin"
-        try:
-            req = urllib2.urlopen(url, None, 3)
-            self.assertTrue( False, "Should have thrown an HTTP Error!")
-        except urllib2.HTTPError as e:
-            self.assertTrue( e.getcode()  != 200 , ("got a  %d" % e.getcode()))
-        else:
-            self.assertTrue( False, "Another Error was thrown!")
-    """
 if __name__ == '__main__':
     unittest.main()
 
